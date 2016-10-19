c0aeb6837d8fdfee52a13440fad5a2fc3840ab9f
leearaneta <lee.araneta@gmail.com>
2016-10-18 21:44:11 -0400

    oops

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :restaurants
   resources :users
   resources :courses
+  post '/check_if_deliverable', to: 'users#check_if_deliverable'
   post '/check_delivery_zone', to: 'restaurants#check_delivery_zone'
   post '/users/validate/fields', to: 'users#validate'
 end
