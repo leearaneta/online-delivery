b0266df2e674be5abc2d10e6f188d889d54dd875
George Akouri <george@philosophie.is>
2016-10-26 21:29:38 -0400

    rails delivering json data

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,14 @@
 class CoursesController < ApplicationController
 
   def index
-    courses = Restaurant.first.courses
+    courses = Restaurant.find_by(name: restaurant_name).courses
     render json: courses, status: 200
+  end
+
+  private
+
+  def restaurant_name
+    request.headers['restaurant'].to_s
   end
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.where(restaurant: Restaurant.first)
+    items = Item.where(restaurant: Restaurant.find_by(name: restaurant_name))
     render json: items, status: 200
+  end
+
+  private
+
+  def restaurant_name
+    request.headers['restaurant'].to_s
   end
 
 end
