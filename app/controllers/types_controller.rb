33df9ac102ad04b9ad357a2fac84c6c20d7b099d
leearaneta <lee.araneta@gmail.com>
2016-11-17 00:19:27 -0500

    ckm

---
+++ app/controllers/types_controller.rb
@@ -0,0 +1,14 @@
+class TypeController < ApplicationController
+
+  def index
+    types = Type.joins(items: :restaurant).where('restaurants.name = ?', restaurant_name).distinct
+    render json: types, status: 200
+  end
+
+  private
+
+  def restaurant_name
+    request.headers['restaurant'].to_s
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :restaurants
   resources :users
   resources :courses
+  resources :types
   post '/check_delivery_zone', to: 'restaurants#check_delivery_zone'
   post '/users/validate/fields', to: 'users#validate'
 end
