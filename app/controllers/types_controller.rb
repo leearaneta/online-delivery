557400675804cf77b6376c0d367e3782acb7a283
George Akouri <george@philosophie.is>
2016-11-18 00:52:07 -0500

    type to types

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,4 +1,4 @@
-class TypeController < ApplicationController
+class TypesController < ApplicationController
 
   def index
     types = Type.joins(items: :restaurant).where('restaurants.name = ?', restaurant_name).distinct
