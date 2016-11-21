edf488bf985a316dfb72635c515ebfcec717a36c
leearaneta <lee.araneta@gmail.com>
2016-11-20 20:58:56 -0500

    remove price from item

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :order_id, :restaurant_id, :course_id, :featured
+  attributes :id, :name, :description, :order_id, :restaurant_id, :course_id, :featured
   has_many :item_types
 end
